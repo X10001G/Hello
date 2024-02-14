7e8756c3167bf4c9bd2bd9bf375085812e78b212
X10001G <142150133+X10001G@users.noreply.github.com>
2024-02-15 00:47:49 +0800

    Create GSXB.js

---
+++ GSXB.js
@@ -0,0 +1,13 @@
+[rewrite_local]
+^https?:\/\/api\.shijianshubao\.com.*?.*? url script-response-body GSXB.js
+
+
+[mitm]
+hostname = api.shijianshubao.com
+%¥
+*******************************/
+var body=$response.body;
+body = body.replace(/isFree\":false/g,'isFree":true');
+body = body.replace(/avatar\":null/g,'avatar":true');
+body = body.replace(/vipEndTimestamp\":\d+/g,'vipEndTimestamp":3705800919000');
+$done(body);
