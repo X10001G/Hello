88a2038a0bb9ed1dad32713eb8c0132da17fdc2f
X10001G <142150133+X10001G@users.noreply.github.com>
2024-03-21 12:40:43 +0800

    Update xykk.js

--- xykk.js
+++ xykk.js
@@ -8,7 +8,7 @@
 *******************************
 %¥
 [rewrite_local]
-^http?:\/\/api\.pollykann\.com.*?|https?:\/\/buy\.itunes\.apple\.com.*?.*? url script-response-body https://github.com/X10001G/Hello/blob/main/xykk.js
+^http?:\/\/api\.pollykann\.com.*?|https?:\/\/buy\.itunes\.apple\.com.*?.*? url script-response-body xykk.js
 
 
 [mitm]
